b7412ba9190a1d812314367cfb5b716080c2ed87
Gabriela de Freitas Maia <20231181110018@ifrn.local>
2025-10-22 17:31:12 -0300

    mudando username para email

--- usuarios/models.py
+++ usuarios/models.py
@@ -3,6 +3,11 @@ from django.contrib.auth.models import AbstractUser
 
 class Usuario(AbstractUser):
     avatar = models.ImageField(upload_to="usuarios-avatar/", blank=True, null=True)
-
+    email = models.EmailField(max_length=255, unique=True)
+   
+    # Define qual o campo é o nome de usuário
+    USERNAME_FIELD = "email"
+    # Necessário para createsuperuser continuar funcionando
+    REQUIRED_FIELDS = ["username"]
     def __str__(self):
         return self.first_name
